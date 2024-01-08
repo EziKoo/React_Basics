d19cbc0a1160275cead3e0a85466b7d7543b80d5
EziKoo <1050864544@qq.com>
2024-01-08 19:50:55 +0800

    评论案例

--- react_18/src/index.js
+++ react_18/src/index.js
@@ -1,14 +1,6 @@
-// 项目的入口 从这里开始运行
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-// React必要的两个核心包
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+const root = createRoot(document.querySelector('#root'))
 
-// 导入项目的根组件
-import App from './App';
-
-// 把App根组件渲染到id为root的dom节点上
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+root.render(<App />)
