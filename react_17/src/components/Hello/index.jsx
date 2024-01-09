0152c09b40e1cc1fb369c80791440ff638cb86b4
Cluckyhy <1050864544@qq.com>
2024-01-09 23:28:03 +0800

    一个简单的Hello组件

---
+++ react_17/src/components/Hello/index.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Hello = () => {
+  return (
+    <h1 className="title">你好呀</h1>
+  )
+}
+
+export default Hello
---
+++ react_17/src/components/Welcome/index.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Welcome = () => {
+  return (
+    <h1 className="welcome">欢迎学习React</h1>
+  )
+}
+
+export default Welcome
