3e5bd0b0a97b3cdbfdfaf14fc5aa269ace89e014
EziKoo <1050864544@qq.com>
2024-01-10 09:31:12 +0800

    更改项目目录结构

--- react_17/src/components/Hello/index.jsx
+++
@@ -1,9 +0,0 @@
-import hello from './index.module.css'
-
-const Hello = () => {
-  return (
-    <h1 className={hello.title}>你好呀</h1>
-  )
-}
-
-export default Hello
--- react_17/src/components/Welcome/index.jsx
+++
@@ -1,9 +0,0 @@
-import wecome from './index.module.css'
-
-const Welcome = () => {
-  return (
-    <h1 className={wecome.title}>欢迎学习React</h1>
-  )
-}
-
-export default Welcome
