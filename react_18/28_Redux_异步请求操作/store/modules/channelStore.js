e69503431c903b46222dc7d3a3f4bafb3059e18f
EziKoo <1050864544@qq.com>
2024-01-11 16:39:59 +0800

    补充对actionCreator中的函数形式，dispatch形参是哪里来的作说明

--- react_18/28_Redux_异步请求操作/store/modules/channelStore.js
+++ react_18/28_Redux_异步请求操作/store/modules/channelStore.js
@@ -18,6 +18,7 @@ const channelStore = createSlice({
 const { setChannels } = channelStore.actions
 const fetchChannelList = () => {
   // 有个疑惑，这里的形参dispatch是哪里来的？？
+  // 因为这里返回的函数是一个回调函数，是由store来调用的，所以store会传入一个dispatch给你
   return async (dispatch) => {
     const res = await axios.get('http://geek.itheima.net/v1_0/channels')
     dispatch(setChannels(res.data.data.channels))
